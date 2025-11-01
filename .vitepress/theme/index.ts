bb75970ae8b2e5a52269d6097beb662f63c46421
sametcn99 <sametcn99@gmail.com>
2025-11-01 04:31:34 +0300

    .

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,4 @@
-import { h, nextTick, watchEffect, watch } from "vue";
+import { h, nextTick, watch } from "vue";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import "./style.css";
@@ -7,30 +7,29 @@ import "vitepress-mermaid-renderer/dist/style.css";
 import { useData, useRouter } from "vitepress";
 
 export default {
-  extends: DefaultTheme,
-  Layout: () => {
-    const { isDark } = useData();
-    const router = useRouter();
+	extends: DefaultTheme,
+	Layout: () => {
+		const { isDark } = useData();
 
-    const initMermaid = () => {
-      const mermaidRenderer = createMermaidRenderer({
-        theme: isDark.value ? "dark" : "forest",
-      });
-      mermaidRenderer.initialize();
-      nextTick(() => mermaidRenderer!.renderMermaidDiagrams());
-    };
+		const initMermaid = () => {
+			const mermaidRenderer = createMermaidRenderer({
+				theme: isDark.value ? "dark" : "forest",
+			});
+			mermaidRenderer.initialize();
+			nextTick(() => mermaidRenderer!.renderMermaidDiagrams());
+		};
 
-    // Initial render
-    nextTick(() => initMermaid());
+		// Initial render
+		nextTick(() => initMermaid());
 
-    // on theme change, re-render mermaid charts
-    watch(
-      () => isDark.value,
-      () => {
-        nextTick(() => initMermaid());
-      },
-    );
+		// on theme change, re-render mermaid charts
+		watch(
+			() => isDark.value,
+			() => {
+				nextTick(() => initMermaid());
+			}
+		);
 
-    return h(DefaultTheme.Layout);
-  },
+		return h(DefaultTheme.Layout);
+	},
 } satisfies Theme;
